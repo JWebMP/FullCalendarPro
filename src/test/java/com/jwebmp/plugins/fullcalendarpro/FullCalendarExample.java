ac009aa2597edaa29e8c5f1fa7abab7169e1e0c8
GedMarc <marc.magon@gmail.com>
2024-03-04 01:27:40 +0200

    Add Font Awesome

--- src/test/java/com/jwebmp/plugins/fullcalendarpro/FullCalendarExample.java
+++ src/test/java/com/jwebmp/plugins/fullcalendarpro/FullCalendarExample.java
@@ -18,32 +18,33 @@ import static com.jwebmp.core.base.angular.client.services.interfaces.Annotation
 
 @NgComponent(value = "full-calendar-example")
 public class FullCalendarExample extends DivSimple<FullCalendarExample>
-		implements INgComponent<FullCalendarExample>
+        implements INgComponent<FullCalendarExample>
 {
-	@Override
-	public void init()
-	{
-		add(new FullCalendarProComponentExample());
-		super.init();
-	}
-	
-	public static void main(String[] args) throws Exception
-	{
-		GuicedUndertow.boot("localhost", 6524);
-	}
-	
-	public void testAppSearch() throws IOException
-	{
-		GuiceContext.inject();
-		for (INgApp<?> app : JWebMPTypeScriptCompiler.getAllApps())
-		{
-			JWebMPTypeScriptCompiler compiler = new JWebMPTypeScriptCompiler(app);
-			
-			System.out.println("Generating @NgApp (" + getTsFilename(app.getClass()) + ") " +
-			                   "in folder " + IComponent.getClassDirectory(app.getClass()));
-			System.out.println("================");
-			//	compiler.renderAppTS(app);
-			System.out.println("================");
-		}
-	}
+    @Override
+    public void init()
+    {
+        add(new FullCalendarProComponentExample());
+        super.init();
+    }
+
+    public static void main(String[] args) throws Exception
+    {
+        GuicedUndertow.boot("localhost", 6524);
+    }
+
+    public void testAppSearch() throws IOException
+    {
+        GuiceContext.instance()
+                    .inject();
+        for (INgApp<?> app : JWebMPTypeScriptCompiler.getAllApps())
+        {
+            JWebMPTypeScriptCompiler compiler = new JWebMPTypeScriptCompiler(app);
+
+            System.out.println("Generating @NgApp (" + getTsFilename(app.getClass()) + ") " +
+                    "in folder " + IComponent.getClassDirectory(app.getClass()));
+            System.out.println("================");
+            //	compiler.renderAppTS(app);
+            System.out.println("================");
+        }
+    }
 }
